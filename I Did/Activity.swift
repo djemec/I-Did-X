85dddc6c0ddbffd4a3c5a69e7fcd099ba05c5a73
DomenJemec <djemec@clemson.edu>
2018-09-02 14:03:01 -0700

    Initiation Push

---
+++ I Did/Activity.swift
@@ -0,0 +1,15 @@
+//
+//  Activity.swift
+//  I Did
+//
+//  Created by Domen B Jemec on 6/9/18.
+//  Copyright © 2018 Jemec Corp. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class Activity: NSManagedObject {
+    
+}
--- I Did/ViewController.swift
+++ I Did/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func clicked(_ sender: UIButton) {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        formatter.dateStyle = .long
+        let timeSelected = formatter.string(from: Date())
+        lastClicked!.text = timeSelected
+        lastVice!.text = "Last - " + (sender.titleLabel?.text)!
+    }
+    
+    @IBOutlet weak var lastClicked: UILabel!
+    @IBOutlet weak var lastVice: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
